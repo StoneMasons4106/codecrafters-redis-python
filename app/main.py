1a1c3025ee25d3c053f92a4a11911ccb2f6560e4
StoneMasons4106 <jake.d.rubach@gmail.com>
2025-08-11 19:32:00 -0400

    Handle concurrent clients.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 import socket  # noqa: F401
+import threading
 
+BUF_SIZE = 4096
+
+def handle_command(client: socket.socket):
+    while chunk := client.recv(BUF_SIZE):
+        if chunk == b"":
+            break
+        # print(f"[CHUNK] ```\n{chunk.decode()}\n```")
+        client.sendall(b"+PONG\r\n")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,14 +17,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
-    
     while True:
-        request: bytes = connection.recv(512)
-        data: str = request.decode()
-
-        if "ping" in data.lower():
-            connection.send("+PONG\r\n".encode())
+        client_socket, client_addr = server_socket.accept()
+        threading.Thread(target=handle_command, args=(client_socket)).start()
 
 
 if __name__ == "__main__":
